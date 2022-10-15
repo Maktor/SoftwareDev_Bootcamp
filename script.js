805d16f1056996b446d9738c4c3925051e4ff6bf
Maktor <66954908+Maktor@users.noreply.github.com>
2022-10-15 13:47:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function yesButton(){
+    alert("i'm from asgardian")
+    alert("now, i will make everything disapear")
+    const element = document.getElementById("mainDiv");
+    element.remove();
+}
+
+function noButton(){
+    const element = document.getElementById("mainDiv");
+    element.remove();
+}
